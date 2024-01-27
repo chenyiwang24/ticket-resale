ec51361729754678705a5cbc6bbda83ede291154
SyedK421767 <syedabdulkarim00@gmail.com>
2024-01-27 16:12:53 -0600

    Searchbar

---
+++ src/pages/SearchBar.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearch = () => {
+    // Pass the searchQuery to the parent component for further processing
+    onSearch(searchQuery);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchBar;
