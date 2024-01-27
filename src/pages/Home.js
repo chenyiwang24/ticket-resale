0eb36ff8e2c5999768036b08f67b39cab383a385
Jefrrey Bennet <jeffrey.a.bennet@gmail.com>
2024-01-27 17:25:45 -0600

    hoierhgore

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import backroundVideo from '../assets/backgroundvideo.mp4';
-import AAIcon from '../assets/AA-icon.png'
 import './Home.css'
 import Navbar from './NavBar';
 
@@ -12,7 +11,7 @@ function Home() {
             </video>
             <div className="overlay"></div>
             <div className="content">
-                <h1 className='title'>No Seat <br /> Wasted</h1>
+                <h1 className='title'>Elevate Your <br /> Journey, Reclaim <br /> the Skies</h1>
                 <p className='description'>Buy and Resell Market</p>
                 <div className='buttons'>
                     <button className='Login'>Login</button>
